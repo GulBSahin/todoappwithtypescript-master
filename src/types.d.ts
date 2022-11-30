8499053cf299ab7b491a5543f1f8f168bfcceff8
GulBSahin <70208660+GulBSahin@users.noreply.github.com>
2022-11-30 22:49:09 +0300

    delete task

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,4 +5,14 @@ interface TodoType {
     task: string;
   };  
 
+  interface ITodoList{
+    todos: TodoType[];
+    toggleTodo : ToggleFn;
+    deleteTodo : DeleteFn;
+  }
+
   type AddFn = (text:string) =>void;
+
+  type ToggleFn = (item:TodoType) =>void;
+
+  type DeleteFn = (item:string|number) =>void;
